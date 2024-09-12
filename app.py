9568b74fc2e5199e85b4f4c19625a7286ba9bee9
michaeladetoro <adetoromichael123@gmail.com>
2024-09-12 16:39:56 +0100

    new

--- app.py
+++ app.py
@@ -7,9 +7,29 @@ from datetime import datetime
 app = Flask(__name__)
 
 # Use the DATABASE_URL environment variable provided by Render
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.secret_key = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///database.db')
+# postgresql://internship_tm9k_user:Xg4JeURoO6n3O6oZblRhbYp3V4DZPCMs@dpg-crhfutrv2p9s73fkesn0-a.oregon-postgres.render.com/internship_tm9k
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+init_db(app)
+
+# /------------------------------------------Welcome------------------------------------------------/
+
+@app.route('/')
+def index():
+    return render_template('welcome.html')
+
+# /------------------------------------------Welcome------------------------------------------------/
+
+# /------------------------------------------Database-----------------------------------------------/
+
+@app.route('/init_db')
+def initialize_database():
+    db.create_all()
+    return 'Database initialized!'
+
+# /------------------------------------------Database-----------------------------------------------/
 init_db(app)
 
 @app.route('/')
